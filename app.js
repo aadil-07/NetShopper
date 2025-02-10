f557191726f762ebdd4f0fdfb28d16d3434b5625
aadil-07 <aadilparwez31122001@gmail.com>
2025-02-10 23:28:32 +0530

    add env file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,7 +12,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb://aadilparwez:pftfaqjbt5YYX8S2@ac-rwkqrvk-shard-00-00.9qnzodn.mongodb.net:27017,ac-rwkqrvk-shard-00-01.9qnzodn.mongodb.net:27017,ac-rwkqrvk-shard-00-02.9qnzodn.mongodb.net:27017/shop?replicaSet=atlas-infulj-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=Cluster0';
+  `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ac-rwkqrvk-shard-00-00.9qnzodn.mongodb.net:27017,ac-rwkqrvk-shard-00-01.9qnzodn.mongodb.net:27017,ac-rwkqrvk-shard-00-02.9qnzodn.mongodb.net:27017/${process.env.MONGO_DATABASE}?replicaSet=atlas-infulj-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
 
 const app = express();
 const store = new MongoDBStore({
@@ -84,7 +85,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(process.env.PORT||3000);
   })
   .catch(err => {
     console.log(err);
